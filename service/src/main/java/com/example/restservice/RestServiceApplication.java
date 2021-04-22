5a1ec7ee473147dd2519e317d2742204d0acacb5
Klassen-Olga <56605448+Klassen-Olga@users.noreply.github.com>
2021-04-22 18:12:46 +0200

    initial commit

---
+++ service/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -0,0 +1,24 @@
+package com.example.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+}
+@RestController
+class MainController{
+
+	@GetMapping("/hello")
+	public String hello(){
+
+		return "hello";
+	}
+}
